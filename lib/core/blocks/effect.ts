d6d4da030ba0e16b572a062c06e38649c48c7b5d
Gleb Fokin <g.fokin@skyeng.ru>
2025-08-11 22:06:39 +0700

    !

--- lib/core/blocks/effect.ts
+++ lib/core/blocks/effect.ts
@@ -1,111 +1,6 @@
 import { NuxMp3PresetIndex } from "lib/core/const";
 import { BlockConfig, ChainItem } from "lib/core/interface";
 
-// EFX: {
-//   types: {
-//     "RC Boost": {
-//       realName: "RC Boost",
-//       params: {
-//         EFX_Para2: { label: "Volume", unit: "%", min: 0, max: 100 }, // default: 40, formatter: ValueFormatters.percentageMPPro
-//         EFX_Para1: { label: "Gain", unit: "%", min: 0, max: 100 }, // default: 35, formatter: ValueFormatters.percentageMPPro
-//         EFX_Para3: { label: "Bass", unit: "%", min: 0, max: 100 }, // default: 35, formatter: ValueFormatters.percentageMPPro
-//         EFX_Para4: { label: "Treble", unit: "%", min: 0, max: 100 }, // default: 70, formatter: ValueFormatters.percentageMPPro
-//       },
-//     },
-//     "AC Boost": {
-//       realName: "AC Boost",
-//       params: {
-//         EFX_Para2: { label: "Volume", unit: "%", min: 0, max: 100 }, // default: 40, formatter: ValueFormatters.percentageMPPro
-//         EFX_Para1: { label: "Gain", unit: "%", min: 0, max: 100 }, // default: 60, formatter: ValueFormatters.percentageMPPro
-//         EFX_Para3: { label: "Bass", unit: "%", min: 0, max: 100 }, // default: 35, formatter: ValueFormatters.percentageMPPro
-//         EFX_Para4: { label: "Treble", unit: "%", min: 0, max: 100 }, // default: 50, formatter: ValueFormatters.percentageMPPro
-//       },
-//     },
-//     "Dist One": {
-//       realName: "Dist One",
-//       params: {
-//         EFX_Para1: { label: "Level", unit: "%", min: 0, max: 100 }, // default: 50, formatter: ValueFormatters.percentageMPPro
-//         EFX_Para3: { label: "Drive", unit: "%", min: 0, max: 100 }, // default: 40, formatter: ValueFormatters.percentageMPPro
-//         EFX_Para2: { label: "Tone", unit: "%", min: 0, max: 100 }, // default: 55, formatter: ValueFormatters.percentageMPPro
-//       },
-//     },
-//     "T Screamer": {
-//       realName: "T Screamer",
-//       params: {
-//         EFX_Para3: { label: "Level", unit: "%", min: 0, max: 100 }, // default: 55, formatter: ValueFormatters.percentageMPPro
-//         EFX_Para1: { label: "Drive", unit: "%", min: 0, max: 100 }, // default: 60, formatter: ValueFormatters.percentageMPPro
-//         EFX_Para2: { label: "Tone", unit: "%", min: 0, max: 100 }, // default: 60, formatter: ValueFormatters.percentageMPPro
-//       },
-//     },
-//     "Blues Drive": {
-//       realName: "Blues Drive",
-//       params: {
-//         EFX_Para1: { label: "Level", unit: "%", min: 0, max: 100 }, // default: 40, formatter: ValueFormatters.percentageMPPro
-//         EFX_Para3: { label: "Gain", unit: "%", min: 0, max: 100 }, // default: 65, formatter: ValueFormatters.percentageMPPro
-//         EFX_Para2: { label: "Tone", unit: "%", min: 0, max: 100 }, // default: 50, formatter: ValueFormatters.percentageMPPro
-//       },
-//     },
-
-//     "Eat Dist": {
-//       realName: "Eat Dist",
-//       params: {
-//         EFX_Para3: { label: "Volume", unit: "%", min: 0, max: 100 }, // default: 60, formatter: ValueFormatters.percentageMPPro
-//         EFX_Para1: { label: "Distortion", unit: "%", min: 0, max: 100 }, // default: 65, formatter: ValueFormatters.percentageMPPro
-//         EFX_Para2: { label: "Filter", unit: "%", min: 0, max: 100 }, // default: 70, formatter: ValueFormatters.percentageMPPro
-//       },
-//     },
-//     "Red Dirt": {
-//       realName: "Red Dirt",
-//       params: {
-//         EFX_Para3: { label: "Level", unit: "%", min: 0, max: 100 }, // default: 50, formatter: ValueFormatters.percentageMPPro
-//         EFX_Para1: { label: "Drive", unit: "%", min: 0, max: 100 }, // default: 65, formatter: ValueFormatters.percentageMPPro
-//         EFX_Para2: { label: "Tone", unit: "%", min: 0, max: 100 }, // default: 50, formatter: ValueFormatters.percentageMPPro
-//       },
-//     },
-//     Crunch: {
-//       realName: "Crunch",
-//       params: {
-//         EFX_Para1: { label: "Volume", unit: "%", min: 0, max: 100 }, // default: 25, formatter: ValueFormatters.percentageMPPro
-//         EFX_Para3: { label: "Gain", unit: "%", min: 0, max: 100 }, // default: 45, formatter: ValueFormatters.percentageMPPro
-//         EFX_Para2: { label: "Tone", unit: "%", min: 0, max: 100 }, // default: 50, formatter: ValueFormatters.percentageMPPro
-//       },
-//     },
-//     "Muff Fuzz": {
-//       realName: "Muff Fuzz",
-//       params: {
-//         EFX_Para1: { label: "Volume", unit: "%", min: 0, max: 100 }, // default: 40, formatter: ValueFormatters.percentageMPPro
-//         EFX_Para3: { label: "Sustain", unit: "%", min: 0, max: 100 }, // default: 40, formatter: ValueFormatters.percentageMPPro
-//         EFX_Para2: { label: "Tone", unit: "%", min: 0, max: 100 }, // default: 50, formatter: ValueFormatters.percentageMPPro
-//       },
-//     },
-//     Katana: {
-//       realName: "Katana",
-//       params: {
-//         EFX_Para2: { label: "Volume", unit: "%", min: 0, max: 100 }, // default: 45, formatter: ValueFormatters.percentageMPPro
-//         EFX_Para1: { label: "Boost", unit: "%", min: 0, max: 100 }, // default: 0, formatter: ValueFormatters.boostModePro
-//       },
-//     },
-//     "ST Singer": {
-//       realName: "ST Singer",
-//       params: {
-//         EFX_Para1: { label: "Volume", unit: "%", min: 0, max: 100 }, // default: 50, formatter: ValueFormatters.percentageMPPro
-//         EFX_Para2: { label: "Gain", unit: "%", min: 0, max: 100 }, // default: 50, formatter: ValueFormatters.percentageMPPro
-//         EFX_Para3: { label: "Filter", unit: "%", min: 0, max: 100 }, // default: 40, formatter: ValueFormatters.percentageMPPro
-//       },
-//     },
-//     "Touch Wah": {
-//       realName: "Touch Wah",
-//       params: {
-//         EFX_Para1: { label: "Type", unit: "%", min: 0, max: 100 }, // default: 1, formatter: ValueFormatters.touchWahFormatterLiteMk2
-//         EFX_Para2: { label: "Wow", unit: "%", min: 0, max: 100 }, // default: 35, formatter: ValueFormatters.percentageMPPro
-//         EFX_Para3: { label: "Sense", unit: "%", min: 0, max: 100 }, // default: 90, formatter: ValueFormatters.percentageMPPro
-//         EFX_Para5: { label: "Level", unit: "%", min: 0, max: 100 }, // default: 100, formatter: ValueFormatters.percentageMPPro
-//         EFX_Para4: { label: "Up/Down Switch", unit: "%", min: 0, max: 100 }, // default: 0, formatter: ValueFormatters.touchWahDirectionFormatterPro
-//       },
-//     },
-//   },
-// },
-
 export enum EffectType {
   DistortionPlus = "Distortion+",
   RCBoost = "RC Boost",
@@ -165,7 +60,49 @@ export const effect: BlockConfig = {
         },
       ],
     },
-    // ...
+
+    //     "RC Boost": {
+    //       realName: "RC Boost",
+    //       params: {
+    //         EFX_Para2: { label: "Volume", unit: "%", min: 0, max: 100 }, // default: 40, formatter: ValueFormatters.percentageMPPro
+    //         EFX_Para1: { label: "Gain", unit: "%", min: 0, max: 100 }, // default: 35, formatter: ValueFormatters.percentageMPPro
+    //         EFX_Para3: { label: "Bass", unit: "%", min: 0, max: 100 }, // default: 35, formatter: ValueFormatters.percentageMPPro
+    //         EFX_Para4: { label: "Treble", unit: "%", min: 0, max: 100 }, // default: 70, formatter: ValueFormatters.percentageMPPro
+    //       },
+    //     },
+    //     "AC Boost": {
+    //       realName: "AC Boost",
+    //       params: {
+    //         EFX_Para2: { label: "Volume", unit: "%", min: 0, max: 100 }, // default: 40, formatter: ValueFormatters.percentageMPPro
+    //         EFX_Para1: { label: "Gain", unit: "%", min: 0, max: 100 }, // default: 60, formatter: ValueFormatters.percentageMPPro
+    //         EFX_Para3: { label: "Bass", unit: "%", min: 0, max: 100 }, // default: 35, formatter: ValueFormatters.percentageMPPro
+    //         EFX_Para4: { label: "Treble", unit: "%", min: 0, max: 100 }, // default: 50, formatter: ValueFormatters.percentageMPPro
+    //       },
+    //     },
+    //     "Dist One": {
+    //       realName: "Dist One",
+    //       params: {
+    //         EFX_Para1: { label: "Level", unit: "%", min: 0, max: 100 }, // default: 50, formatter: ValueFormatters.percentageMPPro
+    //         EFX_Para3: { label: "Drive", unit: "%", min: 0, max: 100 }, // default: 40, formatter: ValueFormatters.percentageMPPro
+    //         EFX_Para2: { label: "Tone", unit: "%", min: 0, max: 100 }, // default: 55, formatter: ValueFormatters.percentageMPPro
+    //       },
+    //     },
+    //     "T Screamer": {
+    //       realName: "T Screamer",
+    //       params: {
+    //         EFX_Para3: { label: "Level", unit: "%", min: 0, max: 100 }, // default: 55, formatter: ValueFormatters.percentageMPPro
+    //         EFX_Para1: { label: "Drive", unit: "%", min: 0, max: 100 }, // default: 60, formatter: ValueFormatters.percentageMPPro
+    //         EFX_Para2: { label: "Tone", unit: "%", min: 0, max: 100 }, // default: 60, formatter: ValueFormatters.percentageMPPro
+    //       },
+    //     },
+    //     "Blues Drive": {
+    //       realName: "Blues Drive",
+    //       params: {
+    //         EFX_Para1: { label: "Level", unit: "%", min: 0, max: 100 }, // default: 40, formatter: ValueFormatters.percentageMPPro
+    //         EFX_Para3: { label: "Gain", unit: "%", min: 0, max: 100 }, // default: 65, formatter: ValueFormatters.percentageMPPro
+    //         EFX_Para2: { label: "Tone", unit: "%", min: 0, max: 100 }, // default: 50, formatter: ValueFormatters.percentageMPPro
+    //       },
+    //     },
 
     {
       label: EffectType.MorningDrive,
@@ -177,6 +114,66 @@ export const effect: BlockConfig = {
         { label: "Tone", encodeIndex: NuxMp3PresetIndex.EFX_Para3 },
       ],
     },
+
+    //     "Eat Dist": {
+    //       realName: "Eat Dist",
+    //       params: {
+    //         EFX_Para3: { label: "Volume", unit: "%", min: 0, max: 100 }, // default: 60, formatter: ValueFormatters.percentageMPPro
+    //         EFX_Para1: { label: "Distortion", unit: "%", min: 0, max: 100 }, // default: 65, formatter: ValueFormatters.percentageMPPro
+    //         EFX_Para2: { label: "Filter", unit: "%", min: 0, max: 100 }, // default: 70, formatter: ValueFormatters.percentageMPPro
+    //       },
+    //     },
+    //     "Red Dirt": {
+    //       realName: "Red Dirt",
+    //       params: {
+    //         EFX_Para3: { label: "Level", unit: "%", min: 0, max: 100 }, // default: 50, formatter: ValueFormatters.percentageMPPro
+    //         EFX_Para1: { label: "Drive", unit: "%", min: 0, max: 100 }, // default: 65, formatter: ValueFormatters.percentageMPPro
+    //         EFX_Para2: { label: "Tone", unit: "%", min: 0, max: 100 }, // default: 50, formatter: ValueFormatters.percentageMPPro
+    //       },
+    //     },
+    //     Crunch: {
+    //       realName: "Crunch",
+    //       params: {
+    //         EFX_Para1: { label: "Volume", unit: "%", min: 0, max: 100 }, // default: 25, formatter: ValueFormatters.percentageMPPro
+    //         EFX_Para3: { label: "Gain", unit: "%", min: 0, max: 100 }, // default: 45, formatter: ValueFormatters.percentageMPPro
+    //         EFX_Para2: { label: "Tone", unit: "%", min: 0, max: 100 }, // default: 50, formatter: ValueFormatters.percentageMPPro
+    //       },
+    //     },
+    //     "Muff Fuzz": {
+    //       realName: "Muff Fuzz",
+    //       params: {
+    //         EFX_Para1: { label: "Volume", unit: "%", min: 0, max: 100 }, // default: 40, formatter: ValueFormatters.percentageMPPro
+    //         EFX_Para3: { label: "Sustain", unit: "%", min: 0, max: 100 }, // default: 40, formatter: ValueFormatters.percentageMPPro
+    //         EFX_Para2: { label: "Tone", unit: "%", min: 0, max: 100 }, // default: 50, formatter: ValueFormatters.percentageMPPro
+    //       },
+    //     },
+    //     Katana: {
+    //       realName: "Katana",
+    //       params: {
+    //         EFX_Para2: { label: "Volume", unit: "%", min: 0, max: 100 }, // default: 45, formatter: ValueFormatters.percentageMPPro
+    //         EFX_Para1: { label: "Boost", unit: "%", min: 0, max: 100 }, // default: 0, formatter: ValueFormatters.boostModePro
+    //       },
+    //     },
+    //     "ST Singer": {
+    //       realName: "ST Singer",
+    //       params: {
+    //         EFX_Para1: { label: "Volume", unit: "%", min: 0, max: 100 }, // default: 50, formatter: ValueFormatters.percentageMPPro
+    //         EFX_Para2: { label: "Gain", unit: "%", min: 0, max: 100 }, // default: 50, formatter: ValueFormatters.percentageMPPro
+    //         EFX_Para3: { label: "Filter", unit: "%", min: 0, max: 100 }, // default: 40, formatter: ValueFormatters.percentageMPPro
+    //       },
+    //     },
+    //     "Touch Wah": {
+    //       realName: "Touch Wah",
+    //       params: {
+    //         EFX_Para1: { label: "Type", unit: "%", min: 0, max: 100 }, // default: 1, formatter: ValueFormatters.touchWahFormatterLiteMk2
+    //         EFX_Para2: { label: "Wow", unit: "%", min: 0, max: 100 }, // default: 35, formatter: ValueFormatters.percentageMPPro
+    //         EFX_Para3: { label: "Sense", unit: "%", min: 0, max: 100 }, // default: 90, formatter: ValueFormatters.percentageMPPro
+    //         EFX_Para5: { label: "Level", unit: "%", min: 0, max: 100 }, // default: 100, formatter: ValueFormatters.percentageMPPro
+    //         EFX_Para4: { label: "Up/Down Switch", unit: "%", min: 0, max: 100 }, // default: 0, formatter: ValueFormatters.touchWahDirectionFormatterPro
+    //       },
+    //     },
+    //   },
+    // },
     // TODO: add other types
   ],
 };
