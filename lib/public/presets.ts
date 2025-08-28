c309a5079e3424d7cc300b74cd53cf31af2b85ed
Gleb Fokin <g.fokin@skyeng.ru>
2025-08-29 01:27:41 +0700

    Add presets

--- lib/public/presets.ts
+++ lib/public/presets.ts
@@ -13,6 +13,14 @@ import sonneGuitarMainRiff from "./preset/sonne-guitar-main-riff";
 import backInBlackGuitarIntro from "./preset/back-in-black-guitar-intro";
 import nothingElseMattersGuitarIntro from "./preset/nothing-else-matters-guitar-intro";
 import panteraFloodsIntro from "./preset/pantera-floods-intro";
+import panteraFloodsSolo from "./preset/pantera-floods-solo";
+import panteraFloodsOutroSolo from "./preset/pantera-floods-outro-solo";
+import oneGuitarIntro from "./preset/one-guitar-intro";
+
+import myWayCleanGuitarIntro from "./preset/my-way-clean-guitar-intro";
+import jimiHendrixHeyJoeWhole from "./preset/jimi-hendrix-hey-joe-whole";
+import scarTissueGuitarIntro from "./preset/rhcp-scar-tissue-guitar-intro";
+import deepPurpleSmokeOnTheWaterGuitarIntro from "./preset/smoke-on-the-water-guitar-intro";
 
 export const presets = [
   sweetHomeAlabamaGuitarIntro,
@@ -30,4 +38,11 @@ export const presets = [
   backInBlackGuitarIntro,
   nothingElseMattersGuitarIntro,
   panteraFloodsIntro,
+  panteraFloodsSolo,
+  panteraFloodsOutroSolo,
+  oneGuitarIntro,
+  myWayCleanGuitarIntro,
+  jimiHendrixHeyJoeWhole,
+  scarTissueGuitarIntro,
+  deepPurpleSmokeOnTheWaterGuitarIntro,
 ];
